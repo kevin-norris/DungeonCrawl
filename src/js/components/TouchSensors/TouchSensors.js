ff80af7421006a81152057c01c381272d911ba2f
Kevin <main@chiptekstudios.com>
2016-10-18 14:38:05 +1300

    Changed size and position of sensors so they worke better

--- src/js/components/TouchSensors/TouchSensors.js
+++ src/js/components/TouchSensors/TouchSensors.js
@@ -18,7 +18,6 @@ class TouchSensors extends React.Component{
 		window.removeEventListener("touchstart", this.handleTouchStart);
 	}
 	processTouch(target){
-		console.log("touched: ", target, " target ID: ", target.id);
 		switch (target.id){
 			case "topSensor":
 				this.props.dispatch(move([-1,0]));
@@ -45,14 +44,14 @@ class TouchSensors extends React.Component{
 	render(){
 		const 	cameraRows = this.props.camera.numRows,
 				cameraCols = this.props.camera.numCols;
-		const height = Math.floor(cameraRows / 3);
-		const width = Math.floor(cameraCols / 3);
+		const height = Math.ceil(cameraRows / 2);
+		const width = Math.ceil(cameraCols / 2);
 		return (
 				<div className="touchSensors">
-					<div id="topSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(0, width, height, width)} key="topSensor" />
-					<div id="botSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height * 2, width, height, width)} key="botSensor" />
-					<div id="leftSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height, 0, height, width)} key="leftSensor" />
-					<div id="rightSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height, width * 2, height, width)} key="rightSensor" />
+					<div id="topSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(0, width / 2, height, width)} key="topSensor" />
+					<div id="botSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height, width / 2, height, width)} key="botSensor" />
+					<div id="leftSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height / 2, 0, height, width / 2)} key="leftSensor" />
+					<div id="rightSensor" className="touchSensor" style={gridCoordsToOffsetAndDimStyle(height / 2, width * 2 - width / 2, height, width / 2)} key="rightSensor" />
 				</div>
 				);
 	}
